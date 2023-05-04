e53461bc9bcf61c6a8d0d029029877803f8ec88f
Himanshu Goyal <68677954+himanshu1109@users.noreply.github.com>
2023-05-04 13:33:57 +0530

    Update config.js

--- SOAS/config/config.js
+++ SOAS/config/config.js
@@ -3,8 +3,8 @@ const config = {
   port: 5000,
   jwtSecret: "YOUR_secret_key",
   mongoUri: "mongodb://localhost:27017/soas",
-  stripe_connect_test_client_id: 'ca_MbbakWEgm2l2dxKlOjXw49pNfhJrmTPK',
-  stripe_test_secret_key: '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',
+  stripe_connect_test_client_id: 'client',
+  stripe_test_secret_key: 'secret',
   stripe_test_api_key: 'pk_live_51LsOIKSGzGsWsC0AUksxnDzaZodiWTPdJjquSl3cZcQ0ogFmJCTMAMjwhRkKIVqafFIPu0R185tG8XyDo4EEwl9E002Zo3m6mW' 
 }
 
